95899295b67b9d2a5aa14ba23a50219378845037
Santosh Kumar <skumar3798@gmail.com>
2023-06-14 00:29:44 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate, createSearchParams } from "react-router-dom"
+
+import  NavBar  from "../src/components/navbar/navar";
+import { Products } from "../src/pages/products/products"
+import { Product } from "../src/pages/product/product"
+import { Cart } from "../src/pages/cart/cart"
+import { NotFound } from "../src/pages/not-found/not-found"
+
+import { useCart } from '../src/context/cart'
 
 function App() {
+
+  const navigate = useNavigate();
+  const { cartItemCount } = useCart()
+
+  const onSearch = (searchQuery) => {
+    navigate(`/?${createSearchParams({ q: searchQuery })}`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar onSearch={onSearch} cartItemCount={cartItemCount()} />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
